7c6b5b9d27a48f4a99aed437caf516bb0fa1c765
comradexlight <comradexlight@gmail.com>
2022-06-06 21:42:16 +0300

    first hw

--- hw1/index.js
+++ hw1/index.js
@@ -1,16 +1,22 @@
-while (true) {
-    const n = Math.round(Math.random());
+let isPlay = true
+while (isPlay) {
+    let n = Math.floor(Math.random() * 1000);
+    console.log(n);
 
-    const userGuess = prompt("Я загадал число, попробуйте угадать (0 - орёл, 1 решка):");
-    
-    if (userGuess === "q") {
-        break;
-    } else if (isNaN(userGuess) || (+userGuess !== 0 && +userGuess !== 1)) {
-        alert("Вы ввели некорректное значение!");
-    } else if (userGuess == n) {
-            alert ("Вы угадали!");
-    } else {
-            alert("Вы не угадали!");
+    while (true) {
+        const userGuess = prompt("Я загадал число в диапазоне от 0 до 999 включительно, попробуйте угадать:");
+        if (userGuess === "exit") {
+            isPlay = false;
+            break;
+        } else if (isNaN(userGuess) || (+userGuess < 0) || (+userGuess > 999)) {
+            alert("Вы ввели некорректное значение!");
+        } else if (userGuess > n){
+            alert("Ваше число больше загаданного, попробуйте ещё!");
+        } else if (userGuess < n){
+            alert("Ваше число меньше загаданного, попробуйте ещё!");
+        }else {
+            alert("Вы угадали!");
+            break;
+        }
     }
 }
-    
