de972089c3b4a4fcd5ebe7b5ca5c3aea6b18c7e3
comradexlight <comradexlight@gmail.com>
2022-06-19 21:45:23 +0300

    from home to work

--- hw3/shop.js
+++ hw3/shop.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Good {
 	constructor (id, name, description, sizes, price, available) {
 		this.id = id;
@@ -11,41 +13,52 @@ class Good {
 	setAvailable(value) {
 		if (typeof value == "boolean") {
 			this.available = value;
+            return this;
 		} else {
 			console.log("Вы ввели неверное значение"); 
 		}
 	};
 };
 
-class GoodsList {
-	constructor (goods, filter, sortPrice, sortDir) {
+class GoodsList{
+    #goods;
+	constructor (goods, sortPrice, sortDir) {
 		this.#goods = goods;
-		this.filter = filter;
+        this.filter =  /name: '{good.name`}'?/gmi;
 		this.sortPrice = sortPrice;
 		this.sortDir = sortDir;
+
 	};
-	
+
 	get list() {
-		return
+        if (this.sortPrice === true) {
+            if (this.sortDir === true) {
+                return this.#goods.sort((good1, good2) => good1.price>good2.price ? 1: -1).filter((good) => good.available === true);
+            }
+            else if (this.sortDir === false) {
+                return this.#goods.sort((good1, good2) => good1.price>good2.price ? -1: 1).filter((good) => good.available === true);
+            };
+        };
 	};
 	
-	add(id) {
-		this.goods.push(id);
+	add(good) {
+		this.#goods.push(good);
 		return;
 	};
 	
 	remove(id) {
-		this.goods.splice(id, 1);
+        this.#goods = this.#goods.filter((good) => good.id !== id);
 		return;
 	};
 }
 
 class BasketGood extends Good {
-	constructor(id, amount) {
-		super.amount = amount;
+	constructor(good, amount){
+        super(good.id, good.name, good.description, good.sizes, good.price, good.available);
+        this.amount = amount;
 	};
 };
-
+/*
 class Basket {
 	constructor(goods) {
 		this.goods = goods;
@@ -54,9 +67,29 @@ class Basket {
 	
 };
 
+*/
+
 let jersey = new Good(0, "jersey", "our new jersey model", "m", 100, true);
-console.log(jersey);
-jersey.setAvailable(false);
-console.log(jersey);
-jersey.setAvailable(123);
-console.log(jersey);
+let cap = new Good(1, "cap", "our new cap model", "m", 40, true);
+let gloves = new Good(2, "gloves", "our standart gloves model", "s", 70, true);
+let pantsWithStraps = new Good(3, "pants with straps", "our standart pants with straps model", "m", 115, true);
+let glasses = new Good(4, "glasses", "a new model of glasses, still in development", "m", 100, false);
+let goodsList = [jersey, cap, gloves, pantsWithStraps, glasses, ];
+//console.log(jersey);
+//jersey.setAvailable(false);
+//console.log(jersey);
+//jersey.setAvailable(123);
+//console.log(jersey);
+//console.log(cap);
+let catalog = new GoodsList(goodsList, true, false);
+//console.log(catalog);
+//console.log(catalog);
+//console.log(catalog.list);
+//catalog.remove(jersey.id);
+//catalog.remove(cap.id);
+//console.log(catalog.list);
+//catalog.add(jersey);
+//catalog.add(cap);
+//console.log(catalog.list);
+let jerseyInBasket = new BasketGood(jersey, 2);
+console.log(jerseyInBasket);
