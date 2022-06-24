c20fb20facbf415219ce85a9a1ba736cd7ad1912
comradexlight <comradexlight@gmail.com>
2022-06-24 09:28:33 +0300

    from home to work

--- hw4/guessNumber.js
+++ hw4/guessNumber.js
@@ -1,27 +1,31 @@
 const rl = require("readline").createInterface(process.stdin, process.stdout);
 
-trueNumber = Number(Math.floor(Math.random() * 100));
+const trueNumber = Number(Math.floor(Math.random() * 100));
 console.log("Я загадал число от 0 до 100. Угадаешь?");
 let counter = 1;
+let logShema = [];
 
 function question() {
 	rl.question("Введи своё число: ", (cmd) => {
-		console.log();
 		if (cmd == trueNumber) {
 			console.log("Бинго! Это верный ответ.\nКоличество попыток", counter);
+            logShema.push(cmd);
 			rl.close();
 			return
 		}
 		else if (cmd > trueNumber) {
 			console.log("Твоё число", cmd, "Это больше загаданного.\nЭто попытка номер", counter);
-			counter++
+            logShema.push(cmd);
+            counter++
 		}
 		else if (cmd < trueNumber) {
 			console.log("Твоё число", cmd, "Это меньше загаданного.\nЭто попытка номер", counter);
+            logShema.push(cmd);
 			counter++
 		}
-		else if (cmd.typeof !== Number) {
+		else if (typeof(cmd) !== Number) {
 			console.log("Вы ввели неверное значение. Введите число от 0 до 100");
+            logShema.push(cmd);        
 		};
 		question();
 	});
